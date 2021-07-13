e04f15ba7b8b96917b929afa44410fb8f0857bcc
arlenwh-code <arlenwh@protonmail.com>
2021-07-12 22:58:28 -0400

    added moment.js behavior

--- js/index.js
+++ js/index.js
@@ -1,47 +1,79 @@
 $('document').ready(function(){
-    $('.to-do-1').text(localStorage.getItem("hr-1"));
-    $('.to-do-2').text(localStorage.getItem("hr-2"));
-    $('.to-do-3').text(localStorage.getItem("hr-3"));
-    $('.to-do-4').text(localStorage.getItem("hr-4"));
-    $('.to-do-5').text(localStorage.getItem("hr-5"));
-    $('.to-do-6').text(localStorage.getItem("hr-6"));
-    $('.to-do-7').text(localStorage.getItem("hr-7"));
-    $('.to-do-8').text(localStorage.getItem("hr-8"));
-    $('.to-do-9').text(localStorage.getItem("hr-9"));
+    $('.to-do-1').text(localStorage.getItem("hour-1"));
+    $('.to-do-2').text(localStorage.getItem("hour-2"));
+    $('.to-do-3').text(localStorage.getItem("hour-3"));
+    $('.to-do-4').text(localStorage.getItem("hour-4"));
+    $('.to-do-5').text(localStorage.getItem("hour-5"));
+    $('.to-do-6').text(localStorage.getItem("hour-6"));
+    $('.to-do-7').text(localStorage.getItem("hour-7"));
+    $('.to-do-8').text(localStorage.getItem("hour-8"));
+    $('.to-do-9').text(localStorage.getItem("hour-9"));
+    checkTime();
 })
+var m = moment();
+var time = ['.time-1','.time-2', '.time-3', '.time-4', '.time-5', '.time-6', '.time-7', '.time-8', '.time-9'];
+var toDo = ['.to-do-1', '.to-do-2', '.to-do-3', '.to-do-4', '.to-do-5', '.to-do-6', '.to-do-7', '.to-do-8', '.to-do-9'];
+var currentHr = m.hour();
+var scheduleTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var hours = ['hour-1', 'hour-2', 'hour-3', 'hour-4', 'hour-5', 'hour-6', 'hour-7', 'hour-8', 'hour-9'];
+var saveBtn = ['.save-btn-1', '.save-btn-2', '.save-btn-3', '.save-btn-4', '.save-btn-5', '.save-btn-6', '.save-btn-7', '.save-btn-8', '.save-btn-9'];
+console.log(moment().hour(currentHr) > moment().hour(9));
+
+$('.current-time').text(moment().format("dddd MMMM DD"));
+
+
+var checkTime = function(){
+    
+    for(var i = 0; i <= 8; i++){
+        $(time[i]).text(m.hour(scheduleTime[i]).format("hh a"));
+        
+
+        if ($(time[i]).text() === moment().format('hh a')){
+            $(toDo[i]).css("background-color", "#FFEAC9");
+        }else if(moment().hour(currentHr) > moment().hour(scheduleTime[i])){
+            $(toDo[i]).css("background-color", "#66DE93");
+        }else if(moment().hour(currentHr) < moment().hour(scheduleTime[i])){
+            $(toDo[i]).css("background-color", "#C6B4CE");
+        };
+        
+    };
+};
+
+
+
 
 $('.save-btn-1').on("click", function(){
-    localStorage.setItem("hr-1", $('.to-do-1').text());
+    localStorage.setItem("hour-1", $('.to-do-1').text());
 });
 
 $('.save-btn-2').on("click", function(){
-    localStorage.setItem("hr-2", $('.to-do-2').text());
+    localStorage.setItem("hour-2", $('.to-do-2').text());
 });
 
 $('.save-btn-3').on("click", function(){
-    localStorage.setItem("hr-3", $('.to-do-3').text());
+    localStorage.setItem("hour-3", $('.to-do-3').text());
 });
 
 $('.save-btn-4').on("click", function(){
-    localStorage.setItem("hr-4", $('.to-do-4').text());
+    localStorage.setItem("hour-4", $('.to-do-4').text());
 });
 
 $('.save-btn-5').on("click", function(){
-    localStorage.setItem("hr-5", $('.to-do-5').text());
+    localStorage.setItem("hour-5", $('.to-do-5').text());
 });
 
 $('.save-btn-6').on("click", function(){
-    localStorage.setItem("hr-6", $('.to-do-6').text());
+    localStorage.setItem("hour-6", $('.to-do-6').text());
 });
 
 $('.save-btn-7').on("click", function(){
-    localStorage.setItem("hr-7", $('.to-do-7').text());
+    localStorage.setItem("hour-7", $('.to-do-7').text());
 });
 
 $('.save-btn-8').on("click", function(){
-    localStorage.setItem("hr-8", $('.to-do-8').text());
+    localStorage.setItem("hour-8", $('.to-do-8').text());
 });
 
 $('.save-btn-9').on("click", function(){
-    localStorage.setItem("hr-9", $('.to-do-9').text());
+    localStorage.setItem("hour-9", $('.to-do-9').text());
 });
